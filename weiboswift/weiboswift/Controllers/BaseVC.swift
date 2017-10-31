43bc28e40e0e001fd04c2486b0ecb14fa180e3a1
culing1317 <1179742576@qq.com>
2017-10-31 14:02:34 +0800

    test insert commit twice

--- weiboswift/weiboswift/Controllers/BaseVC.swift
+++ weiboswift/weiboswift/Controllers/BaseVC.swift
@@ -17,6 +17,7 @@ class BaseVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let a = "test"
+        let b = "test1"
         
         
         // Do any additional setup after loading the view.
