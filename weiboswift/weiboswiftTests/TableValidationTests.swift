a229bb8c9378314802ea40b9c30565e731c0a026
culing1317 <1179742576@qq.com>
2017-10-31 11:29:18 +0800

    Sync

---
+++ weiboswift/weiboswiftTests/TableValidationTests.swift
@@ -0,0 +1,17 @@
+//
+//  TableValidationTests.swift
+//  weiboswiftTests
+//
+//  Created by 邹乾玲 on 2017/10/23.
+//  Copyright © 2017年 touchair. All rights reserved.
+//
+
+import UIKit
+import XCTest
+class TableValidationTests: XCTestCase {
+    func testEmptyTableRowAndColumnCount()  {
+//        let table = UITableView()
+        XCTAssertEqual(1, 1,"ydggdbi");
+    }
+    
+}
